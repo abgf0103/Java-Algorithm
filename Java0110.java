23e627f56dd475dc66a4106ca18840a6bc994869
Jong1 <152011487+abgf0103@users.noreply.github.com>
2025-01-10 21:59:32 +0900

    자바 알고리즘
    문자열 정수 형변환

---
+++ Java0110.java
@@ -0,0 +1,10 @@
+public class Java0110 {
+
+    public static int solution(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("1234"));
+    }
+}
