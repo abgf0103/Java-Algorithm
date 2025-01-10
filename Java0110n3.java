9a27456070086fefa7b9865fba48475f4950bfdc
Jong1 <152011487+abgf0103@users.noreply.github.com>
2025-01-10 22:14:40 +0900

    자릿수 더하기

---
+++ Java0110n3.java
@@ -0,0 +1,14 @@
+public class Java0110n3 {
+    public static int solution(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(123));
+    }
+}
