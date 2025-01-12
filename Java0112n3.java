c7f2029e396cd3501392da47a3122b1d958e80d5
Jong1 <152011487+abgf0103@users.noreply.github.com>
2025-01-12 19:27:56 +0900

    약수의 개수와 덧셈

---
+++ Java0112n3.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Java0111n3 {
+    public static int solution(int left, int right) {
+        int answer = 0;
+        for(int i = left; i <=right; i++){
+            int cnt = 0;
+            for(int j = 1; j <= i; j++){
+                if(i % j == 0){
+                    cnt++;
+                }
+            }
+            if(cnt % 2 == 0){
+                answer += i;
+            }else{
+                answer -= i;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(13, 17));
+    }
+}
