b02d127a79a9f6546f8182e7ae21b7e86dfb9239
Jong1 <152011487+abgf0103@users.noreply.github.com>
2025-01-13 02:03:12 +0900

    부족한 금액 계산하기

---
+++ Java0113n2.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Java0111n3 {
+    public static long solution(int price, int money, int count) {
+        long sum = 0;
+        long answer = -1;
+        for(int i=1; i <= count; i++){
+            sum += price*i;
+        }
+        if(sum<money){
+            return 0;
+        }
+        return sum-money;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(3, 20, 4, 10));
+    }
+}
